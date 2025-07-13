1240dd3ffe39b4738fd39e88aa224ffa5b65a86f
c-salhab <charbel.salhab.2003@gmail.com>
2025-07-13 16:12:41 +0200

    build terraform code for azure deployments

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "moodify-vm-rg"
+}
+
+variable "location" {
+  default = "francecentral"
+}
+
+variable "admin_username" {
+  default = "moodifyuser"
+}
+
+variable "admin_password" {
+  default = "Mo0difyPass!"
+}
